990a070fcaba308a011233a8c02290fd2b7cb40a
yermanhs <ingyermanhs@gmail.com>
2022-09-09 10:36:50 -0500

    Actualización homework05

---
+++ 05-JS-IV/homework/prueba.js
@@ -0,0 +1,17 @@
+var alumnos=[
+    {
+        nombre:'Emi',
+        edad:23,
+        hobbies:['correr','dormir','nadar'],
+        amigos:[{nombre:'Martin'},{nombre:'Leo'}]
+    },
+    {
+        nombre:'Franco',
+        edad:21,
+        hobbies:['futbol','golf'],
+        amigos:[{nombre:'Emi'},{nombre:'Matias'},{nombre:'Jimy'}]
+    }
+]
+
+console.log(alumnos[0].hobbies[1])
+console.log(alumnos[1].amigos[2])
