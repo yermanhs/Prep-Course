26b840109c1b27c87db4ada62c299eddaf592d8e
yermanhs <ingyermanhs@gmail.com>
2022-09-09 19:27:50 -0500

    Actualización homework07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,30 +4,40 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre[0].toUpperCase() + nombre.slice(1)
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2)
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
-  //Tu código:
+  //Tu código: array.reduce(function(acumulador, valorActual[, índice[, array]])[, valorInicial])
+  let suma = numeros.reduce(function(acc,curr){
+    return acc + curr
+  })
+  cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  for(var i = 0; i < array.length; i++) {
+    cb(array[i])
+  }
 }
 
 function map(array, cb) {
@@ -35,12 +45,18 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var newArray = array.map(function(x) {
+    return cb(x)
+  })
+  return newArray
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
-  //Tu código:
+  //Tu código: var newArray = arr.filter(callback(currentValue[, index[, array]])[, thisArg])
+  var nuevoArray = array.filter(inicio => inicio[0]==='a')
+  return nuevoArray
 }
 
 // No modificar nada debajo de esta línea
